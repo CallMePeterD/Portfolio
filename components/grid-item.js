f284c98a5458e0b2d52bccec971aa77a11c14a33
CallMePeterD <pedrobrezil@gmail.com>
2024-06-22 16:41:12 -0300

    alteracoes works

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,20 +1,23 @@
 import NextLink from 'next/link'
 import Image from 'next/image'
-import { Box, Text, LinkBox, LinkOverlay } from '@chakra-ui/react'
+import { Box, Text, LinkBox, LinkOverlay, Badge } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 
-export const GridItem = ({ children, href, title, thumbnail }) => (
+export const GridItem = ({ children, href, tecnologia, thumbnail }) => (
     <Box w="100%" textAlign="center">
       <LinkBox cursor="pointer">
         <Image
           src={thumbnail}
-          alt={title}
+          alt={tecnologia}
           className="grid-item-thumbnail"
           placeholder="blur"
-          loading="lazy"
+          loading="lazy" 
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"         
         />
         <LinkOverlay href={href} target="_blank">
-          <Text mt={2}>{title}</Text>
+          <Text mt={2}>{tecnologia}</Text>
         </LinkOverlay>
         <Text fontSize={14}>{children}</Text>
       </LinkBox>
@@ -22,25 +25,67 @@ export const GridItem = ({ children, href, title, thumbnail }) => (
   )
 
 
-  export const WorkGridItem = ({ children, id, title, thumbnail }) => (
-    <Box w="100%" textAlign="center">
-      <NextLink href={`/works/${id}`} scroll={false} passHref>
-        <LinkBox cursor="pointer">
-          <Image
-            src={thumbnail}
-            alt={title}
-            className="grid-item-thumbnail"
-            placeholder="blur"
-          />
-          <LinkOverlay href={`/works/${id}`} passHref>
-            <Text mt={2} fontSize={20}>
-              {title}
-            </Text>
-          </LinkOverlay>
-          <Text fontSize={14}>{children}</Text>
-        </LinkBox>
-      </NextLink>
+  export const WorkGridItem = ({ children, link, tecnologia, thumbnail }) => (
+    // <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden'>
+    //   <NextLink href={`${link}`} target='_blank' scroll={false} passHref>
+    //   <Image
+    //         src={thumbnail}
+    //         alt={title}
+    //         className="grid-item-thumbnail"
+    //         placeholder="blur"
+    //       />
+        
+    //     <LinkBox cursor="pointer">
+    //       <Image
+    //         src={thumbnail}
+    //         alt={title}
+    //         className="grid-item-thumbnail"
+    //         placeholder="blur"
+    //       />
+    //       <LinkOverlay href={`${link}`}  passHref>
+    //         <Text mt={2} fontSize={20}>
+    //           {title}
+    //         </Text>
+    //       </LinkOverlay>
+    //       <Text fontSize={14}>{children}</Text>
+    //     </LinkBox>
+    //   </NextLink>
+    // </Box>
+    <NextLink href={`${link}`} target='_blank' scroll={false} passHref>
+    <a target='_blank'rel="noopener noreferrer">
+    <Box maxW='sm' borderWidth='1px' borderRadius='lg' overflow='hidden' cursor="pointer">
+    <Image src={thumbnail} alt={thumbnail} />
+
+    <Box p='6'>
+      <Box display='flex' alignItems='baseline'>    
+      <Badge borderRadius='full' px='2' colorScheme='teal'>
+            {tecnologia}
+          </Badge>   
+        <Box
+          color='gray.500'
+          fontWeight='semibold'
+          letterSpacing='wide'
+          fontSize='xs'
+          textTransform='uppercase'
+          ml='2'
+        >          
+        </Box>
+      </Box>
+      <Box
+        mt='1'
+        fontWeight='semibold'
+        as='h4'
+        lineHeight='tight'
+        noOfLines={1}
+      >
+        {children}
+      </Box>
+      <Box>
+      </Box>      
     </Box>
+  </Box>
+  </a>
+  </NextLink>
   )
   
   export const GridItemStyle = () => (
---
+++ pages/works.js
@@ -0,0 +1,57 @@
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import Section from '../components/section';
+import { WorkGridItem } from '../components/grid-item';
+import montyhall from '../public/images/works/MontyHall.png';
+import galinha from '../public/images/works/galinha.png';
+import netflix from '../public/images/works/netflix.png';
+import flask from '../public/images/works/flask.png';
+//import breve from '../public/images/works/Design_sem_nome__1_-removebg-preview.png'
+
+
+const Works = () => {
+    return (
+        <Container>
+            <Heading as='h3' fontSize={20} mb={4}>
+                Projetos
+            </Heading>
+            {/* <Heading as='h4' fontSize={16} mb={4}>
+                Front-End
+            </Heading> */}
+            <SimpleGrid columns={[1,1,2]} gap={6}>
+                
+                <Section>                
+                <WorkGridItem link={'https://monty-hall-next-js.vercel.app/'} tecnologia="NextJS" thumbnail={montyhall}>
+                        MontyHall
+                    </WorkGridItem>
+                </Section>
+                
+                <Section>
+                <WorkGridItem link={'https://callmepeterd.github.io/netflix/'} tecnologia="HTML/CSS" thumbnail={netflix}>
+                        Clone Netflix
+                    </WorkGridItem>
+                </Section>
+            </SimpleGrid>
+            <Heading as='h3' fontSize={16} mb={4}>
+                Python
+            </Heading>
+            <SimpleGrid columns={[1,1,2]} gap={6}>
+            <Section>
+                <WorkGridItem link='https://callmepeterd.itch.io/save-the-chicken' tecnologia="Python" thumbnail={galinha}>
+                        Projeto de Faculdade usando Python
+                    </WorkGridItem>
+                </Section>
+                <Section>
+                <WorkGridItem link='https://github.com/CallMePeterD/SimpleFlaskBlog' tecnologia="Flask" thumbnail={flask}>
+                        Blog Simples
+                    </WorkGridItem>
+                </Section>
+
+            </SimpleGrid>
+        </Container>
+    )
+}
+
+
+
+
+export default Works
